58c8624d353a215495e012f1d29791f3ab41301a
Santiago Isaza Morales <159985540+saisazamo@users.noreply.github.com>
2025-10-06 17:07:31 -0500

    Attempt 1: Lab_01

--- homework/homework.py
+++ homework/homework.py
@@ -92,3 +92,161 @@
 # {'type': 'cm_matrix', 'dataset': 'train', 'true_0': {"predicted_0": 15562, "predicte_1": 666}, 'true_1': {"predicted_0": 3333, "predicted_1": 1444}}
 # {'type': 'cm_matrix', 'dataset': 'test', 'true_0': {"predicted_0": 15562, "predicte_1": 650}, 'true_1': {"predicted_0": 2490, "predicted_1": 1420}}
 #
+
+import pandas as pd
+import numpy as np
+import json
+import os
+import gzip
+import pickle
+import joblib
+import zipfile
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import (
+    precision_score, recall_score, f1_score,
+    balanced_accuracy_score, confusion_matrix
+)
+
+
+train = pd.read_csv(
+    "files/input/train_data.csv.zip",  # archivo comprimido en .zip
+    index_col=False,                      # no usar ninguna columna como índice
+    compression="zip",                    # indicar que está comprimido
+)
+
+test = pd.read_csv(
+    "files/input/test_data.csv.zip",  # archivo comprimido en .zip
+    index_col=False,                      # no usar ninguna columna como índice
+    compression="zip",                    # indicar que está comprimido
+)
+
+
+# Renombrar la variable target
+train.rename(columns={"default payment next month": "default"}, inplace=True)
+test.rename(columns={"default payment next month": "default"}, inplace=True)
+
+# Eliminar ID
+if "ID" in train.columns:
+    train.drop(columns=["ID"], inplace=True)
+    test.drop(columns=["ID"], inplace=True)
+    
+
+#Eliminar filas con NA
+train.dropna(inplace=True)
+test.dropna(inplace=True)
+
+
+# EDUCATION > 4 => 4 (others)
+train.loc[train["EDUCATION"] > 4, "EDUCATION"] = 4
+test.loc[test["EDUCATION"] > 4, "EDUCATION"] = 4
+
+
+# =======================
+# Paso 2. División X/y
+# =======================
+X_train = train.drop(columns=["default"])
+y_train = train["default"]
+
+X_test = test.drop(columns=["default"])
+y_test = test["default"]
+
+# =======================
+# Paso 3. Pipeline
+# =======================
+categorical_cols = ["SEX", "EDUCATION", "MARRIAGE",
+                    "PAY_0", "PAY_2", "PAY_3", "PAY_4", "PAY_5", "PAY_6"]
+
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_cols)
+    ],
+    remainder="passthrough"
+)
+
+rf = RandomForestClassifier(random_state=42, n_jobs=-1)
+
+pipe = Pipeline(steps=[
+    ("preprocessor", preprocessor),
+    ("model", rf)
+])
+
+# =======================
+# Paso 4. Optimización
+# =======================
+param_grid = {
+    "model__n_estimators": [100, 300, 500],
+    "model__max_depth": [10, 20, None],
+    "model__min_samples_split": [2, 5]
+}
+
+grid = GridSearchCV(
+    pipe,
+    param_grid=param_grid,
+    cv=10,
+    scoring="balanced_accuracy",
+    n_jobs=-1,
+    verbose=2
+)
+
+grid.fit(X_train, y_train)
+
+
+# =======================
+# Paso 5. Guardar modelo
+# =======================
+
+best_model_path = 'files/models'
+os.makedirs(best_model_path, exist_ok=True)
+
+with gzip.open(os.path.join(best_model_path, "model.pkl.gz"), "wb") as f:
+    pickle.dump(grid, f)
+    
+
+# =======================
+# Paso 6. Métricas
+# =======================
+def compute_metrics(y_true, y_pred, dataset_name):
+    return {
+        "type": "metrics",
+        "dataset": dataset_name,
+        "precision": precision_score(y_true, y_pred),
+        "balanced_accuracy": balanced_accuracy_score(y_true, y_pred),
+        "recall": recall_score(y_true, y_pred),
+        "f1_score": f1_score(y_true, y_pred)
+    }
+
+y_train_pred = grid.predict(X_train)
+y_test_pred = grid.predict(X_test)
+
+metrics = []
+metrics.append(compute_metrics(y_train, y_train_pred, "train"))
+metrics.append(compute_metrics(y_test, y_test_pred, "test"))
+
+# =======================
+# Paso 7. Confusion Matrix
+# =======================
+def compute_conf_matrix(y_true, y_pred, dataset_name):
+    cm = confusion_matrix(y_true, y_pred, labels=[0,1])
+    return {
+        "type": "cm_matrix",
+        "dataset": dataset_name,
+        "true_0": {"predicted_0": int(cm[0,0]), "predicted_1": int(cm[0,1])},
+        "true_1": {"predicted_0": int(cm[1,0]), "predicted_1": int(cm[1,1])}
+    }
+
+metrics.append(compute_conf_matrix(y_train, y_train_pred, "train"))
+metrics.append(compute_conf_matrix(y_test, y_test_pred, "test"))
+
+metrics_path = 'files/output/'
+os.makedirs(metrics_path, exist_ok=True)
+
+# Guardar JSON
+with open("files/output/metrics.json", "w") as f:
+    for m in metrics:
+        f.write(json.dumps(m) + "\n")
